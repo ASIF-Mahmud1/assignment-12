df7b20e099b0e5b687dd5a3d612261d97bb3699d
Asif Mahmud <1421015@iub.edu.bd>
2018-03-19 03:44:08 +0000

    practice

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var app =require('express')();
 var http = require('http').Server(app);
+var io =require('socket.io')(http);
 
 app.get('/', function(req,res)
 {
    res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket)
+{
+     console.log('Listening on *:3000');
+});
+
 http.listen(process.env.PORT, function()
 {
     console.log('Listening on *:3000');
 });
+
+
 
